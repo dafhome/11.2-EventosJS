7659307f44d370962120f468f7ca121414f5bd93
dafhome <d.fernalvarez@gmail.com>
2024-03-19 21:36:06 +0100

    commit

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 
-const color = ["red", "yellow", "blue", "white", "green", "grey"];
+const color = ["red", "yellow", "blue", "white", "green", "grey", "black"];
 let contador = 0;
 
 function cambiarColor(event){
     let key = event.key;
+
     if (contador<color.length-1){
         document.getElementById("input-text").style.backgroundColor = color[contador];
         contador++;
+        console.log(contador);
     }
     else{
         document.getElementById("input-text").style.backgroundColor = color[contador];
         contador=0;
+        console.log(contador);
     }
     
 }
